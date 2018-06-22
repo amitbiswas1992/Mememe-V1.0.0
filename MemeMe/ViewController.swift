44d17d6820e6312da919ca3981d977a7b71c1130
Amit Biswas <biswas1992@hotmail.com>
2018-06-22 17:35:33 -0400

    added UIImagePickerControllerdelegate  to the class declaration

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,7 +7,7 @@
 //
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     
     @IBOutlet weak var imagePickerView: UIImageView!
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     @IBAction func pickAnImage(_ sender: Any) {
         let pickerController = UIImagePickerController()
         present(pickerController, animated: true, completion: nil)
+        //dismiss(animated: true, completion: nil)
 
     }
     
